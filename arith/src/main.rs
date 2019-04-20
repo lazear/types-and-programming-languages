76e7c925979caec60c6f6a96c2a36660b419e46b
Michael Lazear <lazear@scripps.edu>
2019-04-19 19:28:16 -0700

    Parser error handling

--- arith/src/main.rs
+++ arith/src/main.rs
@@ -6,9 +6,15 @@ use lexer::Lexer;
 use parser::{Parser, Term};
 
 fn main() {
-    let input = "iszero(succ(succ(succ(0))))";
+    let input = "succ(succ(succ(succ(0))))";
     let mut p = Parser::new(input);
     while let Some(tm) = p.parse_term() {
         println!("{:?}", tm);
     }
+
+    let diag = p.diagnostic();
+    if diag.error_count() > 0 {
+        println!("\n{} errors detected while parsing!", diag.error_count());
+        println!("{}", diag.emit());
+    }    
 }
--- arith/src/parser.rs
+++ arith/src/parser.rs
@@ -15,14 +15,60 @@ pub enum Term {
 
 pub struct Parser<'s> {
     src: &'s str,
+    diagnostic: Diagnostic<'s>,
     lexer: Peekable<Lexer<'s>>,
     span: Span,
 }
 
-impl Parser<'_> {
-    pub fn new(input: &str) -> Parser<'_> {
+pub struct Diagnostic<'s> {
+    src: &'s str,
+    messages: Vec<String>,
+    spans: Vec<Span>,
+}
+
+impl Diagnostic<'_> {
+    pub fn new(src: &str) -> Diagnostic<'_> {
+        Diagnostic {
+            src,
+            messages: Vec::new(),
+            spans: Vec::new(),
+        }
+    }
+
+    pub fn push<S: Into<String>>(&mut self, msg: S, span: Span) {
+        self.messages.push(msg.into());
+        self.spans.push(span);
+    }
+
+    pub fn error_count(&self) -> usize {
+        self.messages.len()
+    }
+
+    pub fn emit(mut self) -> String {
+        let mut s = String::new();
+        assert_eq!(self.messages.len(), self.spans.len());
+
+        let lines = self.src.lines().collect::<Vec<&str>>();
+        for i in 0 .. self.messages.len() {
+            let msg = &self.messages[i];
+            let span = self.spans[i];
+
+            s.push_str(&format!("Error occuring at line {}, col: {}: {}\n{}\n{}^~~~\n", span.start.line, span.start.col, msg, 
+                &lines[span.start.line as usize],
+                (0..span.start.col).map(|_| ' ').collect::<String>(),
+            ));
+        }
+        self.messages.clear();
+        self.spans.clear();
+        s
+    }
+}
+
+impl<'s> Parser<'s> {
+    pub fn new(input: &'s str) -> Parser<'s> {
         Parser {
             src: input,
+            diagnostic: Diagnostic::new(input),
             lexer: Lexer::new(input.chars()).peekable(),
             span: Span::default(),
         }
@@ -74,14 +120,20 @@ impl Parser<'_> {
             Token::Semicolon => return self.parse_term(),
             Token::Int(x) => baptize(x),
             Token::Then | Token::Else | Token::RParen => {
-                panic!("out of place token! @ {:?}", self.span)
+                //panic!("out of place token! @ {:?}", self.span)
+                self.diagnostic.push("Out of place token", self.span);
+                return self.parse_term()
             }
-            Token::Invalid => panic!(
-                "Invalid token! {}",
-                &self.src[self.span.start.abs as usize..self.span.end.abs as usize]
-            ),
+            Token::Invalid => {
+                self.diagnostic.push("Invalid token", self.span);
+                return self.parse_term()
+            }
         };
         Some(kind)
+    }
+
+    pub fn diagnostic(self) -> Diagnostic<'s> {
+        self.diagnostic
     }
 }
 
@@ -106,6 +158,14 @@ fn sin(term: Term) -> Option<u32> {
         }
     }
     None
+}
+
+impl Drop for Diagnostic<'_> {
+    fn drop(&mut self) {
+        if self.error_count() != 0 {
+            panic!("parser::Diagnostic dropped without handling {} errors!", self.error_count());
+        }
+    }
 }
 
 #[cfg(test)]
